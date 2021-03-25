9515807737d48ab08f51404f41a7565c41279904
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-25 11:54:03 +0100

    cleaning interface

--- include/elbow.h
+++ include/elbow.h
@@ -4,10 +4,7 @@
 #include <vector>
 
 namespace elbow {
-
-float secondDerivative(std::vector<float>& x);
-float firstDerivative(const std::vector<float>& x);
-float analyze(const std::vector<float>& x);
+float analyze(std::vector<float>& x);
 
 }
 #endif /* ELBOW_H */
